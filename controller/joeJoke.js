07b49dc680768f329ec0589fff846e64c2fb257e
Ruturaj <ruturajtawde@gmail.com>
2021-08-04 19:17:37 +0530

    rng chance to joe joke

--- controller/joeJoke.js
+++ controller/joeJoke.js
@@ -1,7 +1,9 @@
 module.exports = (msg, arg) => {
     if (msg.author.username != 'Dota2') {
         if (msg.content.toUpperCase().includes('JOE')) {
-            msg.channel.send("Joe mama!!");
+            if(Math.floor(Math.random()*10) < 3){
+                msg.channel.send("Joe mama!!");
+            }
         } else {
             msg.reply("Why don't you ask Joe??");
         }
