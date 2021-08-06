8b6d88d0b39d3462dfc6c5405ff1e88eb149adc7
Ruturaj <ruturajtawde@gmail.com>
2021-08-06 22:35:55 +0530

    fix for probability

--- controller/joeJoke.js
+++ controller/joeJoke.js
@@ -1,11 +1,11 @@
 module.exports = (msg, arg) => {
     if (msg.author.username != 'Dota2') {
         if (msg.content.toUpperCase().includes('JOE')) {
-            if(Math.floor(Math.random()*10) < 3){
-                msg.channel.send("Joe mama!!");
-            }
+            msg.channel.send("Joe mama!!");
         } else {
-            msg.reply("Why don't you ask Joe??");
+            if (Math.floor(Math.random() * 10) < 4) {
+                msg.reply("Why don't you ask Joe??");
+            }
         }
     }
 }
